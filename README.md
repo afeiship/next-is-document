30e95a3e1ad82142942c6502bb4e7135f08312e5
afeiship <afeiship@gmail.com>
2019-01-15 09:12:09 +0800

    feat: int& done

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-is-document
+> Check if is windowDocument for next
+
+## install:
+```bash
+npm install -S afeiship/next-is-document --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
---
+++ src/next-is-document.js
@@ -0,0 +1,14 @@
+(function () {
+
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+
+  nx.isDocument = function (inObj) {
+    return inObj != null && inObj.nodeType == 9;
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.isDocument;
+  }
+
+}());
